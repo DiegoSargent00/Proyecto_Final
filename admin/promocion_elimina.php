56b285235594daafe3106ff4c630f87166915a02
DiegoSargent <54731217+DiegoSargent@users.noreply.github.com>
2023-12-02 23:48:54 -0600

    Add files via upload

--- admin/promocion_elimina.php
+++ admin/promocion_elimina.php
@@ -2,21 +2,10 @@
 require "funciones/conecta.php";
 
 if (isset($_REQUEST['id']) && is_numeric($_REQUEST['id'])) {
-    $con = conecta();
-    $id = $_REQUEST['id'];
-    
-    // Verificar si la promoción está asociada a algún pedido antes de eliminarla
-    $sqlCheck = "SELECT COUNT(*) FROM pedidos WHERE id_promocion = ?";
-    $stmtCheck = $con->prepare($sqlCheck);
-    $stmtCheck->bind_param("i", $id);
-    $stmtCheck->execute();
-    $stmtCheck->bind_result($count);
-    $stmtCheck->fetch();
-    $stmtCheck->close();
+    try {
+        $con = conecta();
+        $id = $_REQUEST['id'];
 
-    if ($count > 0) {
-        echo "No se puede eliminar la promoción porque está asociada a uno o más pedidos.";
-    } else {
         $sql = "UPDATE promociones SET eliminado = 1 WHERE id = ?";
         $stmt = $con->prepare($sql);
 
@@ -24,15 +13,17 @@ if (isset($_REQUEST['id']) && is_numeric($_REQUEST['id'])) {
             $stmt->bind_param("i", $id);
 
             if ($stmt->execute()) {
-                echo $id;
+                echo json_encode(["success" => true, "id" => $id]);
             } else {
-                echo "Error al eliminar la promoción.";
+                throw new Exception("Error al eliminar la promoción.");
             }
         } else {
-            echo "Error en la consulta SQL.";
+            throw new Exception("Error en la consulta SQL.");
         }
+    } catch (Exception $e) {
+        echo json_encode(["error" => $e->getMessage()]);
     }
 } else {
-    echo "ID no válido o no proporcionado.";
+    echo json_encode(["error" => "ID no válido o no proporcionado."]);
 }
 ?>
