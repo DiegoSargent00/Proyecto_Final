b836e9809e22e494419a20ada8121e9f4b18add9
DiegoSargent <54731217+DiegoSargent@users.noreply.github.com>
2023-12-02 23:13:27 -0600

    Add files via upload
    base de datos con tabla promociones

--- empresa01.sql
+++ empresa01.sql
@@ -1,9 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3307
+-- Tiempo de generación: 03-12-2023 a las 06:12:57
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE DATABASE IF NOT EXISTS `empresa01`;
-USE `empresa01`;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `empresa01`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
 
 CREATE TABLE `empleado` (
   `id` int(11) NOT NULL,
@@ -67,7 +89,34 @@ CREATE TABLE `productos` (
 
 INSERT INTO `productos` (`id`, `nombre`, `codigo`, `descripcion`, `costo`, `stock`, `archivo_n`, `archivo`, `status`, `eliminado`) VALUES
 (1, 'Teclado', '1', 'Teclado RGB ', 400, 19, '', 'cd66b6cc2c2afd8d7beede7d16e8ae15.jpg', 1, 1),
-(2, 'Mouse RGB', '2', 'Mouse RGB ideal para jugadores', 200, 10, '', '2d2dc66088474f81f6c8bdbf2857d657.jpg', 1, 1);
+(2, 'Mouse RGB', '2', 'Mouse RGB ideal para jugadores', 200, 10, '', '2d2dc66088474f81f6c8bdbf2857d657.jpg', 1, 1),
+(3, 'Computadora de oficina', '3', 'Computadora de oficina ideal para espacios reducidos', 3000, 10, '', '6d399319ed7ddf18e63b0f76a7721a6c.jpg', 1, 0),
+(4, 'Mousepad', '123', 'Mousepad RGB', 200, 64, '', 'ecb907f9ef93661d4f1f3e572da16509.jpg', 1, 0),
+(5, 'Teclado', '4', 'Teclado para computadora RGB', 600, 10, '', 'ecb907f9ef93661d4f1f3e572da16509.jpg', 1, 0),
+(6, 'Camara para computadora', '56', 'Cámara para hacer llamadas', 250, 8, '', '6d399319ed7ddf18e63b0f76a7721a6c.jpg', 1, 1),
+(7, 'Audifonos', '10', 'Audífonos RGB', 300, 8, '', '82768a7c419c96af1dd9a7cf44cc8e7d.jpg', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `promociones`
+--
+
+CREATE TABLE `promociones` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(128) NOT NULL,
+  `archivo` varchar(64) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT 1,
+  `eliminado` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `promociones`
+--
+
+INSERT INTO `promociones` (`id`, `nombre`, `archivo`, `status`, `eliminado`) VALUES
+(1, 'Audifonos RGB', '82768a7c419c96af1dd9a7cf44cc8e7d.jpg', 1, 0),
+(2, 'Camara', '6d399319ed7ddf18e63b0f76a7721a6c.jpg', 1, 0);
 
 --
 -- Índices para tablas volcadas
@@ -88,6 +137,12 @@ ALTER TABLE `productos`
   ADD KEY `id` (`id`);
 
 --
+-- Indices de la tabla `promociones`
+--
+ALTER TABLE `promociones`
+  ADD KEY `id` (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -101,6 +156,12 @@ ALTER TABLE `empleado`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `promociones`
+--
+ALTER TABLE `promociones`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
